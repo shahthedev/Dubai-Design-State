641e907742d59df769bfe1eeb9ff775a58153a56
Shahzore <shahzoreasif2@gmail.com>
2025-08-15 23:03:34 +0500

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/Dubai-Design-State/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/Dubai-Design-State/' : '/',
   plugins: [react()],
-})
+}))
